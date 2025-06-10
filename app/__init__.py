2fba4b157a7b56c6fb442e2c0e0b0d0c0201ec06
Simon André Ruth <simon.andre.ruth@uni-weimar.de>
2025-06-10 10:07:03 +0200

    trying to change to a logic so we start from last data point in db

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 # app.py
-from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
 import os
-from pathlib import Path
 
 app = Flask(__name__, template_folder='../templates')
 app.secret_key = 'supersecretkey'
@@ -19,7 +18,7 @@ class InputData(db.Model):
     image_path = db.Column(db.String)
     story_string = db.Column(db.String)
     crux = db.Column(db.String)
-    important_characters_and_objects = db.Column(db.Text)  # JSON as string
+    important_characters_and_objects = db.Column(db.Text)
 
 class Annotation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -44,48 +43,32 @@ def annotate():
     if not input_data:
         return "No input data available."
 
-    if request.method == 'GET' and 'image_index' not in session:
-        session['image_index'] = 0
-        session['concept_index'] = 0
+    for image_idx, row in enumerate(input_data):
+        objects = json.loads(row.important_characters_and_objects)
+        match = Annotation.query.filter_by(file_name=row.file_name, image_path=row.image_path).first()
 
-    image_idx = session.get('image_index', 0)
-    concept_idx = session.get('concept_index', 0)
+        stored_objects = []
+        if match:
+            stored_objects = json.loads(match.important_characters_and_objects)
 
-    if image_idx >= len(input_data):
+        for concept_idx, obj in enumerate(objects):
+            if len(stored_objects) <= concept_idx or 'user_present' not in stored_objects[concept_idx]:
+                break
+        else:
+            continue
+        break
+    else:
         return render_template('done.html')
 
-    row = input_data[image_idx]
-    objects = json.loads(row.important_characters_and_objects)
-
-    match = Annotation.query.filter_by(file_name=row.file_name, image_path=row.image_path).first()
-    if match:
-        stored_objects = json.loads(match.important_characters_and_objects)
-        for i in range(min(len(objects), len(stored_objects))):
-            if 'user_present' in stored_objects[i]:
-                objects[i]['user_present'] = stored_objects[i]['user_present']
-
+    # Update annotation on POST
     if request.method == 'POST':
-        action = request.form.get('action')
-
-        if action == 'prev':
-            if concept_idx > 0:
-                session['concept_index'] = concept_idx - 1
-            else:
-                if image_idx > 0:
-                    session['image_index'] = image_idx - 1
-                    row = input_data[image_idx - 1]
-                    objects = json.loads(row.important_characters_and_objects)
-                    match = Annotation.query.filter_by(file_name=row.file_name, image_path=row.image_path).first()
-                    if match:
-                        stored_objects = json.loads(match.important_characters_and_objects)
-                        for i in range(min(len(objects), len(stored_objects))):
-                            if 'user_present' in stored_objects[i]:
-                                objects[i]['user_present'] = stored_objects[i]['user_present']
-                    session['concept_index'] = len(objects) - 1
-            return redirect(url_for('annotate'))
-
         present = request.form.get('present') == 'yes'
         objects[concept_idx]['user_present'] = present
+
+        if stored_objects:
+            for i in range(min(len(objects), len(stored_objects))):
+                if 'user_present' in stored_objects[i]:
+                    objects[i]['user_present'] = stored_objects[i]['user_present']
 
         existing = Annotation.query.filter_by(file_name=row.file_name, image_path=row.image_path).first()
         if existing:
@@ -103,24 +86,25 @@ def annotate():
             db.session.add(ann)
         db.session.commit()
 
-        session['concept_index'] = concept_idx + 1
-        if session['concept_index'] >= len(objects):
-            session['image_index'] = image_idx + 1
-            session['concept_index'] = 0
-
         return redirect(url_for('annotate'))
 
-    concept = objects[concept_idx]
     image_path = url_for('custom_image', filename=row.image_path)
+    concept = objects[concept_idx]
 
     return render_template('index.html',
                            image_path=image_path,
                            story=row.story_string,
                            concept=concept,
-                           image_num=image_idx+1,
-                           concept_num=concept_idx+1,
+                           image_num=image_idx + 1,
+                           concept_num=concept_idx + 1,
                            total_images=len(input_data),
                            total_concepts=len(objects))
+
+@app.route('/reset_db')
+def reset_db():
+    Annotation.query.delete()
+    db.session.commit()
+    return redirect(url_for('annotate'))
 
 if __name__ == '__main__':
     with app.app_context():
