6967da2f6454e389fdc9f89ded1d3142de848dde
Simon Ruth <63604385+Simrut@users.noreply.github.com>
2025-06-11 11:18:05 +0200

    Db nav (#2)
    * trying to improve db nav
    
    * trying to adapt to db scheme
    
    * not able to deal with db scheme yet
    
    * improvement in navigation
    
    * when going back to first image, not reaching 'all annotated'
    
    * only annotating when going forward

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
-# app.py
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import json
 import os
 
 app = Flask(__name__, template_folder='../templates')
@@ -18,16 +16,41 @@ class InputData(db.Model):
     image_path = db.Column(db.String)
     story_string = db.Column(db.String)
     crux = db.Column(db.String)
-    important_characters_and_objects = db.Column(db.Text)
+    object_name = db.Column(db.String)
+    user_present = db.Column(db.Boolean, default=None, nullable=True)
 
 class Annotation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     file_name = db.Column(db.String, index=True)
     image_path = db.Column(db.String)
-    story_string = db.Column(db.String)
-    crux = db.Column(db.String)
-    important_characters_and_objects = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime)
+    object_name = db.Column(db.String)
+    user_present = db.Column(db.Boolean, default=None, nullable=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+def get_annotatable(direction='forward', current_id=None):
+    query = InputData.query.order_by(InputData.id)
+    records = query.all()
+    ids = [r.id for r in records]
+
+    if not ids:
+        return None
+
+    if current_id is None:
+        return ids[0] if direction == 'forward' else ids[-1]
+
+    try:
+        idx = ids.index(current_id)
+        next_idx = idx + 1 if direction == 'forward' else idx - 1
+
+        if next_idx < 0:
+            return ids[0]  # Stay on first image
+        elif next_idx >= len(ids):
+            return None  # End of images
+        else:
+            return ids[next_idx]
+    except ValueError:
+        return None
+
 
 @app.route('/custom_images/<filename>')
 def custom_image(filename):
@@ -39,72 +62,74 @@ def index():
 
 @app.route('/annotate', methods=['GET', 'POST'])
 def annotate():
-    input_data = InputData.query.order_by(InputData.id).all()
-    if not input_data:
-        return "No input data available."
+    current_id = request.args.get('id', type=int)
+    direction = request.form.get('action') if request.method == 'POST' else request.args.get('direction', 'forward')
 
-    for image_idx, row in enumerate(input_data):
-        objects = json.loads(row.important_characters_and_objects)
-        match = Annotation.query.filter_by(file_name=row.file_name, image_path=row.image_path).first()
-
-        stored_objects = []
-        if match:
-            stored_objects = json.loads(match.important_characters_and_objects)
-
-        for concept_idx, obj in enumerate(objects):
-            if len(stored_objects) <= concept_idx or 'user_present' not in stored_objects[concept_idx]:
-                break
-        else:
-            continue
-        break
-    else:
-        return render_template('done.html')
-
-    # Update annotation on POST
     if request.method == 'POST':
         present = request.form.get('present') == 'yes'
-        objects[concept_idx]['user_present'] = present
+        input_id = request.form.get('input_id')
 
-        if stored_objects:
-            for i in range(min(len(objects), len(stored_objects))):
-                if 'user_present' in stored_objects[i]:
-                    objects[i]['user_present'] = stored_objects[i]['user_present']
+        if input_id is None:
+            return "Missing input_id", 400
 
-        existing = Annotation.query.filter_by(file_name=row.file_name, image_path=row.image_path).first()
-        if existing:
-            existing.important_characters_and_objects = json.dumps(objects)
-            existing.timestamp = datetime.utcnow()
-        else:
-            ann = Annotation(
-                file_name=row.file_name,
-                image_path=row.image_path,
-                story_string=row.story_string,
-                crux=row.crux,
-                important_characters_and_objects=json.dumps(objects),
-                timestamp=datetime.utcnow()
-            )
-            db.session.add(ann)
-        db.session.commit()
+        input_id = int(input_id)
+        record = InputData.query.get(input_id)
 
-        return redirect(url_for('annotate'))
+        if record:
+            ann = Annotation.query.filter_by(file_name=record.file_name, image_path=record.image_path, object_name=record.object_name).first()
+            if not ann:
+                ann = Annotation(
+                    file_name=record.file_name,
+                    image_path=record.image_path,
+                    object_name=record.object_name,
+                )
+                db.session.add(ann)
 
+            if direction =="forward":
+                ann.user_present = present
+                ann.timestamp = datetime.utcnow()
+                db.session.commit()
+            current_id = input_id
+
+    next_id = get_annotatable(direction, current_id)
+    if next_id is None:
+        return render_template('done.html')
+
+    row = InputData.query.get(next_id)
+    annotated = Annotation.query.filter_by(file_name=row.file_name, image_path=row.image_path, object_name=row.object_name).first()
     image_path = url_for('custom_image', filename=row.image_path)
-    concept = objects[concept_idx]
+
+    total_concepts = InputData.query.count()
+    total_images = len(set((r.file_name, r.image_path) for r in InputData.query.all()))
+    concept_num = InputData.query.order_by(InputData.id).filter(InputData.id <= row.id).count()
+
+    seen_images = set()
+    image_num = 0
+    for r in InputData.query.order_by(InputData.id):
+        key = (r.file_name, r.image_path)
+        if key not in seen_images:
+            image_num += 1
+            seen_images.add(key)
+        if r.id == row.id:
+            break
 
     return render_template('index.html',
                            image_path=image_path,
                            story=row.story_string,
-                           concept=concept,
-                           image_num=image_idx + 1,
-                           concept_num=concept_idx + 1,
-                           total_images=len(input_data),
-                           total_concepts=len(objects))
+                           concept={'name': row.object_name},
+                           image_idx=row.id,
+                           present=annotated.user_present if annotated else None,
+                           input_id=row.id,
+                           image_num=image_num,
+                           concept_num=concept_num,
+                           total_images=total_images,
+                           total_concepts=total_concepts)
 
-@app.route('/reset_db')
-def reset_db():
-    Annotation.query.delete()
-    db.session.commit()
-    return redirect(url_for('annotate'))
+# @app.route('/reset_db')
+# def reset_db():
+#     Annotation.query.delete()
+#     db.session.commit()
+#     return redirect(url_for('annotate'))
 
 if __name__ == '__main__':
     with app.app_context():
